81c857a022737d5fded1b86b86af533d89b7913d
alexv710 <34195476+alexv710@users.noreply.github.com>
2018-03-10 17:57:26 +0100

    test 2

--- data_preparation.py
+++ data_preparation.py
@@ -4,7 +4,11 @@ import matplotlib.pyplot as plt
 import sklearn.preprocessing
 
 
+<<<<<<< HEAD
 plt.style.use('seaborn-whitegrid')
+=======
+print(df_stock[[]['MSFT']])
+>>>>>>> 8e649acd2d517fb222d44c706eab3743bcb65f44
 
 df_stock = pd.read_csv('stock_version_one.csv',).set_index('TICKER')
 
