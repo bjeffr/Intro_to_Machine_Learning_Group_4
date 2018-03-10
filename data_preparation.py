0a0bd7c67e6bd43acf20dcf5adc319d1bfa7a0ed
alexv710 <34195476+alexv710@users.noreply.github.com>
2018-03-10 18:51:07 +0100

    imputing strategy

--- data_preparation.py
+++ data_preparation.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import numpy as np
 #import matplotlib.pyplot as plt
-import sklearn.preprocessing
+from sklearn.preprocessing import Imputer
+
+
 
 df_stock = pd.read_csv('stock_version_one.csv').set_index('TICKER')
 print(df_stock.date['MSFT'])
+
+
 
 #hallo
 print("Du habasch")
@@ -13,3 +17,10 @@ print("Du habasch")
 print(df_stock.isnull().sum())
 
 #Impute missing values
+# The imputation strategy.
+#
+# If “mean”, then replace missing values using the mean along the axis.
+# If “median”, then replace missing values using the median along the axis.
+# If “most_frequent”, then replace missing using the most frequent value along the axis.
+
+ipr = Imputer(missing_values='Na', strategy='')
