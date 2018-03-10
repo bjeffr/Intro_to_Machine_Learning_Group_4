34c394c462b34b1f58b8adc8b76c811fc9fda4c8
alexv710 <34195476+alexv710@users.noreply.github.com>
2018-03-10 22:54:46 +0100

    cleaned up stocks and filled nearly all NaN
    PERMNO    0
    date      0
    SICCD     1
    TICKER    1
    DCLRDT    0
    PAYDT     0
    DIVAMT    1
    FACPR     0
    FACSHR    0
    BIDLO     1
    ASKHI     1
    PRC       1
    VOL       1
    SHROUT    1
    RETX      1
    ewretd    0
    dtype: int64

--- data_preparation.py
+++ data_preparation.py
@@ -22,9 +22,19 @@ df_ratio = pd.read_csv('ratio_version_one.csv', index_col=[0,1])
 #         df_stock.set_value(i, 'PAYDT', '0')
 #     df_stock.set_value(i, 'TICKER', '')
 
+#forwardfill for DIVAMT because $-Value per share of distribution has not only a monthly impact but is rather a constant
+#indicator
+df_stock['DCLRDT'] = df_stock['DCLRDT'].fillna(0)
+df_stock['PAYDT'] = df_stock['PAYDT'].fillna(0)
+df_stock['FACPR'] = df_stock['FACPR'].fillna(0)
+df_stock['FACSHR'] = df_stock['FACSHR'].fillna(0)
+
+df_stock['DIVAMT'] = df_stock['DIVAMT'].ffill()
+
 
 # Drop a column if too many NaN or just considered unimportant
 df_stock = df_stock.drop('HSICMG', axis=1) #has only values for JPM, NKE and CSCO makes no sense to include
+df_stock = df_stock.drop('SPREAD', axis=1)
 print(df_stock.isnull().sum())
 # print(df_ratio.isnull().sum())
 # print(df_stock)
@@ -38,10 +48,9 @@ print(df_stock.isnull().sum())
 # If “median”, then replace missing values using the median along the axis.
 # If “most_frequent”, then replace missing using the most frequent value along the axis.
 
-# ipr = Imputer(missing_values='Na', strategy='mean', axis=0)
+# ipr = Imputer(missing_values='NaN', strategy='mean', axis=0)
 # ipr = ipr.fit(df_stock.values)
 # imputed_data = ipr.transform(df_stock.values)
-
 # same for ratios
 # ipr = Imputer(missing_values='Na', strategy='mean', axis=0)
 # ipr = ipr.fit(df_ratio.values)
