2b95cb7dc7f9db520088da39147e3e6799c2171c
bjeffr <benjamin.jeffrey@outlook.com>
2018-03-10 18:06:55 +0100

    .

--- data_preparation.py
+++ data_preparation.py
@@ -1,12 +1,10 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import sklearn.preprocessing
 
-plt.style.use('seaborn-whitegrid')
-print(df_stock[[]['MSFT']])
-
-df_stock = pd.read_csv('stock_version_one.csv',).set_index('TICKER')
+df_stock = pd.read_csv('stock_version_one.csv').set_index('TICKER')
+print(df_stock.head())
 
 #check for NA values
 print(df_stock.isnull().sum())
