1d70cf91308351e983227ba7f2d78fdc81f49b07
bjeffr <benjamin.jeffrey@outlook.com>
2018-03-10 18:31:41 +0100

    .

---
+++ data_preparation.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+#import matplotlib.pyplot as plt
+import sklearn.preprocessing
+
+df_stock = pd.read_csv('stock_version_one.csv').set_index('TICKER')
+print(df_stock.date['MSFT'])
+
+#hallo
+print("Du habasch")
+
+#check for NA values
+print(df_stock.isnull().sum())
+
+#Impute missing values
