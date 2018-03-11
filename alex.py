ebc41d553cc97b1aa070420582414ff3ba18e170
Alexander Wyss <alexander.wyss@uzh.ch>
2018-03-11 19:39:17 +0100

    aufräumen

--- alex.py
+++ alex.py
@@ -1,2 +1,19 @@
-# converting the date to datetype, works but I don't know how to do it with the index
-# pd.to_datetime(df_stock['date'], dayfirst=True)
+from Intro_to_Machine_Learning_Group_4 import data_preparation_stocks as sp
+
+sp.stock_formatter()
+
+
+# Impute missing values
+# The imputation strategy.
+#
+# If “mean”, then replace missing values using the mean along the axis.
+# If “median”, then replace missing values using the median along the axis.
+# If “most_frequent”, then replace missing using the most frequent value along the axis.
+
+# ipr = Imputer(missing_values='NaN', strategy='most_frequent', axis=0)
+# ipr = ipr.fit(df_stock.values)
+# imputed_data = ipr.transform(df_stock.values)
+# # same for ratios
+# ipr = Imputer(missing_values='Na', strategy='mean', axis=0)
+# ipr = ipr.fit(df_ratio.values)
+# imputed_data = ipr.transform(df_ratio.values)
---
+++ data_preparation_ratios.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+df_ratio = pd.read_csv('ratio_version_one.csv', index_col=[0,1])
