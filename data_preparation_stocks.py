0ec1f599b2dda99c4346cac72b294efee39746a9
alexv710 <34195476+alexv710@users.noreply.github.com>
2018-03-14 18:02:15 +0100

    Decision Trees/Standartization

--- data_preparation_stocks.py
+++ data_preparation_stocks.py
@@ -50,12 +50,20 @@ def stock_formatter():
 
     # delete all rows that contain more than the specified number of NaN values,
     # please only use this as the last step and check the variable row_counter to see how many rows have been deleted
-
     thresh = 3
     row_counter = 0
     for row in df_stock.iterrows():
         if row[1].isnull().sum()>thresh:
             df_stock.drop(df_stock.index[int(row[0])], inplace=True)
             row_counter += 1
-    # print(row_counter)
+
+    stock_col = ['SICCD', 'DIVAMT', 'BIDLO', 'ASKHI', 'PRC', 'VOL', 'SHROUT', 'ewretd']
+    cols = df_stock.columns.tolist()
+    cols = cols[:3] + cols[5:-2] + cols[-1:] + cols[4:5] #+ cols[-2:-1]
+
+    # CAREFUL PERMNO CODE DELETED FOR TESTING REASONS!!!!!!!!!!!!!!!
+
+    df_stock = df_stock[cols[2:]]
+
+
     return df_stock
