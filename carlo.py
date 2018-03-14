3e533af81550fbb4df767999934af3631096652b
Carlo <c.scherrer@gmx.ch>
2018-03-14 18:06:43 +0100

    .

--- carlo.py
+++ carlo.py
@@ -1,8 +1,13 @@
-<<<<<<< HEAD
-=======
+from Intro_to_Machine_Learning_Group_4 import data_preparation_stocks as sp
+import numpy as np
+import pandas as pd
+
 from Intro_to_Machine_Learning_Group_4.data_preparation_ratios import ratio_formatter
 
+#Funktion die mir angibt wie viele NaN Werte in den Ratios vorkommen
 df_ratio = ratio_formatter()
-
 print(df_ratio.isnull().sum())
->>>>>>> 378271e372902fb05b9084fca030c6748b0960b6
+
+
+
+
