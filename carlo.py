378271e372902fb05b9084fca030c6748b0960b6
alexv710 <34195476+alexv710@users.noreply.github.com>
2018-03-14 14:42:58 +0100

    .

--- carlo.py
+++ carlo.py
@@ -1,4 +1,5 @@
+from Intro_to_Machine_Learning_Group_4.data_preparation_ratios import ratio_formatter
 
+df_ratio = ratio_formatter()
 
-
-print(df_stock.isnull().sum())
+print(df_ratio.isnull().sum())
--- data_preparation_ratios.py
+++ data_preparation_ratios.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+def ratio_formatter():
 
-
-df_ratio = pd.read_csv('ratio_version_one.csv', index_col=[0,1])
+    df_ratio = pd.read_csv('ratio_version_one.csv', index_col=[0,1])
+    return df_ratio
