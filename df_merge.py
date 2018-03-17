058319c311064ed476b1bb653f283a192f1bb696
alexv710 <34195476+alexv710@users.noreply.github.com>
2018-03-17 17:06:23 +0100

    .

--- df_merge.py
+++ df_merge.py
@@ -1,22 +1,30 @@
 from Intro_to_Machine_Learning_Group_4.data_preparation_ratios import ratio_formatter
 from Intro_to_Machine_Learning_Group_4.data_preparation_stocks import stock_formatter
+import pandas as pd
 
-df_ratio = ratio_formatter()
-df_stock = stock_formatter()
+)
+df_ratio = pd.read_csv('ratio_version_one.csv')
+count = 0
+for i, n in df_stock['date'].iteritems():
+    print(i, n)
+print(df_ratio[1])
+
+# print(df_stock['date'])
+# print(df_ratio['public_date'])
 
 
-print(df_ratio.shape)
-
-print(df_stock.shape)
-
-df = df_stock.merge(df_ratio, left_on=['permno', 'date'], right_on=['permno', 'public_date'], how='left')
-
-
-print(df.shape)
-
-df = df.drop('date', axis=1)
-df = df.drop('permno', axis=1)
+# print(df_ratio.shape)
 #
-print(df.isnull().sum())
+# print(df_stock.shape)
 #
-# print(df.columns)
+# df = df_stock.merge(df_ratio, left_on=['permno', 'date'], right_on=['permno', 'public_date'], how='left')
+#
+#
+# print(df.shape)
+#
+# df = df.drop('date', axis=1)
+# df = df.drop('permno', axis=1)
+# #
+# print(df.isnull().sum())
+# #
+# # print(df.columns)
